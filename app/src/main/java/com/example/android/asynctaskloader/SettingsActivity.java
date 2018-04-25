8e2bb2283f7dc2099bc28bce511d47b78fde2d14
Alex Ferreira <arf92@live.com>
2018-04-25 08:26:24 -0300

    Finished the exer

---
+++ app/src/main/java/com/example/android/asynctaskloader/SettingsActivity.java
@@ -0,0 +1,16 @@
+package com.example.android.asynctaskloader;
+
+import android.example.com.visualizerpreferences.R;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+}
